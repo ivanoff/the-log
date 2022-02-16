12b009b1e0e9d238c138b21f96559ec79a43c62c
Dimitry Ivanov <2@ivanoff.org.ua>
2022-02-17 01:14:14 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # the-log
+
+*The Log is dead. Long live the Log!*
+
+### Usage
+
+```javascript
+require('the-log');
+
+console.log('echo'); // [2022-02-16T22:06:42.566Z] echo
+```
+
+### To-do
+
+require('the-log')({ timestamp: true, stringify: true });
+
+require('the-log')('{timestamp} {id} {log}');
+
+require('the-log')((name, ...arr) => (arr.map(log =>({ timestamp, name, log })));
---
+++ src/index.js
@@ -0,0 +1,19 @@
+(c => {
+  const timestamp = new Date().toISOString()
+
+  console = {
+    ...c,
+    log: (...arr) => c.log(`[${timestamp}]`, ...arr),
+    warn: (...arr) => c.warn(`[${timestamp}]`, ...arr),
+    count: (...arr) => c.count(`[${timestamp}]`, ...arr),
+    group: (...arr) => c.group(`[${timestamp}]`, ...arr),
+    debug: (...arr) => c.debug(`[${timestamp}]`, ...arr),
+    info: (...arr) => c.info(`[${timestamp}]`, ...arr),
+    error: (...arr) => c.error(`[${timestamp}]`, ...arr),
+    timeLog: (t, ...arr) => c.timeLog(t, `[${timestamp}]`, ...arr),
+    timeEnd: (t, ...arr) => {
+      c.timeLog(t, `[${timestamp}]`, ...arr);
+      c.timeEnd(t, ...arr);
+    },
+  }
+})(console);
