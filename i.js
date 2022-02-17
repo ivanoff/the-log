62bfb22febe6a12c79b19f53485f3ff35d44436c
Dimitry Ivanov <2@ivanoff.org.ua>
2022-02-17 09:38:13 +0200

    fix timestamp

---
+++ i.js
@@ -0,0 +1,19 @@
+require('./src');
+
+console.log('echo');
+const a = console.log('one two free');
+
+console.log(a);
+console.log(console);
+
+  console.warn('a', 'warn');
+  console.count('a', 'count');
+  console.count('a', 'count');
+  console.group('a', 'group');
+  console.debug('a', 'debug');
+  console.info('a', 'info');
+  console.error('a', 'error');
+
+  console.time('x', 'a');
+  console.timeLog('x', 'a', 'timeLog');
+  console.timeEnd('x', 'a', 'timeEnd');
